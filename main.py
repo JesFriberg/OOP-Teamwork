9b2173fbe92134851b9ed46030bdb089508f343a
Jesse Friberg <jesse.friberg@edu.turkuamk.fi>
2022-04-04 11:16:58 +0300

    created the main file

---
+++ main.py
@@ -0,0 +1,6 @@
+# File: main.py
+# Author: Jesse Friberg and Joonatan Silvennoinen
+# Description: main file for the program that allows you to keep track of your trading cards
+
+from card import Card, Mtg_card, Pokemon_card, Yugioh_card
+from player import Player
